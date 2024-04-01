f631a54762edd85295923503a234364ade836798
hardiksaraiya2203 <hardiksaraiya2203@gmail.com>
2024-04-01 14:29:32 +0530

    Construction Site

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const Image=document.querySelector('.BrandLogoA');
+
+function chnagebg(){
+    Image.src="image/ProminaOrange.png";
+};
+
+function chnageBack(){
+    Image.src="image/ProminaWhite.png";
+};
